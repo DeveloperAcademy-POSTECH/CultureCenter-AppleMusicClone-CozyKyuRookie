08d7fd0d428d46c27fd98763d0e69f10145ed504
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-07-02 15:06:24 +0900

    feat: initial commit

---
+++ AppleMusicClone/AppleMusicCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicCloneApp.swift
+//  AppleMusicClone
+//
+//  Created by Hankyu Lee on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
