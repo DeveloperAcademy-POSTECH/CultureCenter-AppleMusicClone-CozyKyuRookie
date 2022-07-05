5eec5416185568b28ad63d319fd1a0ec1550494d
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-07-05 22:26:22 +0900

    #7 feat: fetch data ViewModel 추가

--- AppleMusicClone/Views/SearchViews/SearchView.swift
+++ AppleMusicClone/Views/SearchViews/SearchView.swift
@@ -30,7 +30,9 @@ struct SearchView: View {
 //                            self.vmForArtists.fetchData(text: location)
 //                            self.vmForPlaylists.fetchData(text: location)
                             
-                            searchVM.fetchData(location)
+//                            searchVM.fetchData(location)
+                            
+                            searchVM.fetch(location)
                             
                         }
                         .padding()
@@ -39,8 +41,14 @@ struct SearchView: View {
                 LazyVGrid(columns: [GridItem(.flexible())]) {
                     LazyVStack {
                         Text("Hints")
-                        ForEach(searchVM.hints.results.suggestions.indices, id: \.self) { index in
-                            Text(searchVM.hints.results.suggestions[index].displayTerm)
+                        ForEach(
+//                            searchVM.hints.results.suggestions.indices
+                            searchVM.hints3.indices
+                            , id: \.self) { index in
+                            Text(
+//                                searchVM.hints.results.suggestions[index].displayTerm
+                                searchVM.hints3[index].inform
+                            )
                         }
                     }
                     Rectangle()
@@ -50,11 +58,17 @@ struct SearchView: View {
                     //songs
                     LazyVStack {
                         Text("songs")
-                        ForEach(searchVM.songs.results.suggestions.indices, id: \.self) { index in
+                        ForEach(
+//                            searchVM.songs.results.suggestions.indices
+                            searchVM.songs3.indices
+                            , id: \.self) { index in
                             
                             HStack {
                               
-                                AsyncImage(url: searchVM.getImageUrl(urlString: searchVM.songs.results.suggestions[index].content.attributes.artwork.url)) { image in
+                                AsyncImage(url:
+//                                            searchVM.getImageUrl(urlString: searchVM.songs.results.suggestions[index].content.attributes.artwork.url)
+                                           searchVM.songs3[index].imageUrl
+                                ) { image in
                                     image
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
@@ -63,7 +77,8 @@ struct SearchView: View {
                                 }
                                 .frame(width: 50, height: 50)
                                 
-                                Text(searchVM.songs.results.suggestions[index].content.attributes.name)
+//                                Text(searchVM.songs.results.suggestions[index].content.attributes.name)
+                                Text(searchVM.songs3[index].inform)
                                 Spacer()
                             }
                             
@@ -77,12 +92,18 @@ struct SearchView: View {
                     //playlists
                     LazyVStack {
                         Text("playlists")
-                        ForEach(searchVM.playlists.results.suggestions.indices, id: \.self) { index in
+                        ForEach(
+//                            searchVM.playlists.results.suggestions.indices
+                            searchVM.playlists3.indices
+                                , id: \.self) { index in
                             
                             
                             HStack {
                               
-                                AsyncImage(url: searchVM.getImageUrl(urlString: searchVM.playlists.results.suggestions[index].content.attributes.artwork.url)) { image in
+                                AsyncImage(url:
+                                            searchVM.playlists3[index].imageUrl
+//                                            searchVM.getImageUrl(urlString: searchVM.playlists.results.suggestions[index].content.attributes.artwork.url)
+                                ) { image in
                                     image
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
@@ -91,7 +112,8 @@ struct SearchView: View {
                                 }
                                 .frame(width: 50, height: 50)
                                 
-                                Text(searchVM.playlists.results.suggestions[index].content.attributes.name)
+//                                Text(searchVM.playlists.results.suggestions[index].content.attributes.name)
+                                Text(searchVM.playlists3[index].inform)
                                 Spacer()
                             }
                         }
@@ -102,10 +124,18 @@ struct SearchView: View {
                     //artist
                     LazyVStack {
                         Text("artists")
-                        ForEach(searchVM.artists.results.suggestions.indices, id: \.self) { index in
+                        ForEach(
+//                            searchVM.artists.results.suggestions.indices
+                            searchVM.artists3.indices
+                            , id: \.self) { index in
                             HStack {
                               
-                                AsyncImage(url: searchVM.getImageUrl(urlString: searchVM.artists.results.suggestions[index].content.attributes.artwork.url)) { image in
+                                AsyncImage(url:
+                                            searchVM.artists3[index].imageUrl
+//                                            searchVM.getImageUrl(urlString:
+//                                                                        searchVM.artists.results.suggestions[index].content.attributes.artwork.url
+//                                                                     searchVM.artists3[index].urlString)
+                                ) { image in
                                     image
                                         .resizable()
                                         .aspectRatio(contentMode: .fit)
@@ -114,7 +144,10 @@ struct SearchView: View {
                                 }
                                 .frame(width: 50, height: 50)
                                 
-                                Text(searchVM.artists.results.suggestions[index].content.attributes.name)
+                                Text(
+                                    searchVM.artists3[index].inform
+//                                    searchVM.artists.results.suggestions[index].content.attributes.name
+                                )
                                 Spacer()
                             }
                             
@@ -137,10 +170,50 @@ struct SearchView_Previews: PreviewProvider {
 }
 
 
+//searchVM.playlists.results.suggestions[index].content.attributes.artwork.url
 
-struct HintViewModel {
+struct commonViewModel<T> {
+    
+    let suggestion: T
+    let inform: String
+    var imageUrl: URL?
+    
+    init(suggestion: T) {
+        self.suggestion = suggestion
+        switch suggestion {
+        case let hint as SuggestionHints :
+            inform = hint.displayTerm
+        case let songs as Suggestion2:
+            inform = songs.content.attributes.name
+            imageUrl = getImageUrl(urlString: songs.content.attributes.artwork.url)
+        case let artist as Suggestion3:
+            inform = artist.content.attributes.name
+            imageUrl = getImageUrl(urlString: artist.content.attributes.artwork.url)
+            
+        case let playlist as Suggestion4:
+            inform = playlist.content.attributes.name
+            imageUrl = getImageUrl(urlString: playlist.content.attributes.artwork.url)
+        default:
+            inform = ""
+        }
+    }
+    private func getImageUrl(urlString: String) -> URL? {
+        //        "https://is2-ssl.mzstatic.com/image/thumb/Music124/v4/02/32/09/02320995-35fb-e63f-fb52-76595b70ed45/1.jpg/{w}x{h}bb.jpg"
+                var path  = urlString
+                path = path.replacingOccurrences(of: "{w}", with: "100")
+                path = path.replacingOccurrences(of: "{h}", with: "100")
+                
+                guard let encodedString = path.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+                    return nil
+                }
+                guard let url = URL(string: encodedString) else {
+                    return nil
+                }
+        return url
+            }
     
 }
+
 class SearchViewModel: ObservableObject {
     
     private var cancellableForPlaylists: AnyCancellable?
@@ -148,31 +221,60 @@ class SearchViewModel: ObservableObject {
     private var cancellableForArtists: AnyCancellable?
     private var cancellableForHints: AnyCancellable?
     
-    @Published var hintsViewModel: HintViewModel = HintViewModel()
+    @Published var hints3: [commonViewModel<SuggestionHints>] = []
+    @Published var playlists3: [commonViewModel<Suggestion4>] = []
+    @Published var artists3: [commonViewModel<Suggestion3>] = []
+    @Published var songs3: [commonViewModel<Suggestion2>] = []
     
-    @Published var hints: WelcomeHints = WelcomeHints.placeholder()
-    @Published var playlists: Welcome4 = Welcome4.placeholder()
-    @Published var artists: Welcome3 = Welcome3.placeholder()
-    @Published var songs: Welcome2 = Welcome2.placeholder()
+//    @Published var hints: WelcomeHints = WelcomeHints.placeholder()
+//    @Published var playlists: Welcome4 = Welcome4.placeholder()
+//    @Published var artists: Welcome3 = Welcome3.placeholder()
+//    @Published var songs: Welcome2 = Welcome2.placeholder()
     
-    func fetchData(_ text: String) {
+    func fetch(_ text: String) {
         self.cancellableForHints = WebService.searchData(resource: Resource<WelcomeHints>(urlString: getHintsUrlString(text)))
             .catch { _ in Just(WelcomeHints.placeholder())}
-            .assign(to: \.hints, on: self)
+            .sink(receiveValue: { hints in
+                self.hints3 = hints.results.suggestions.map{ commonViewModel.init(suggestion: $0)}
+            })
         
         self.cancellableForSongs = WebService.searchData(resource: Resource<Welcome2>(urlString: getSongsUrlString(text)))
             .catch { _ in Just(Welcome2.placeholder())}
-            .assign(to: \.songs, on: self)
+            .sink(receiveValue: { songs in
+                self.songs3 = songs.results.suggestions.map{ commonViewModel.init(suggestion: $0)}
+            })
         
         self.cancellableForArtists = WebService.searchData(resource: Resource<Welcome3>(urlString: getArtistsUrlString(text)))
             .catch { _ in Just(Welcome3.placeholder())}
-            .assign(to: \.artists, on: self)
-        
+            .sink(receiveValue: { artists in
+                self.artists3 = artists.results.suggestions.map{ commonViewModel.init(suggestion: $0)}
+            })
         self.cancellableForPlaylists = WebService.searchData(resource: Resource<Welcome4>(urlString: getPlaylistsUrlString(text)))
             .catch { _ in Just(Welcome4.placeholder())}
-            .assign(to: \.playlists, on: self)
-        
+            .sink(receiveValue: { playlists in
+                self.playlists3 = playlists.results.suggestions.map{ commonViewModel.init(suggestion: $0)}
+            })
     }
+    
+    
+//    func fetchData(_ text: String) {
+//        self.cancellableForHints = WebService.searchData(resource: Resource<WelcomeHints>(urlString: getHintsUrlString(text)))
+//            .catch { _ in Just(WelcomeHints.placeholder())}
+//            .assign(to: \.hints, on: self)
+//        
+//        self.cancellableForSongs = WebService.searchData(resource: Resource<Welcome2>(urlString: getSongsUrlString(text)))
+//            .catch { _ in Just(Welcome2.placeholder())}
+//            .assign(to: \.songs, on: self)
+//        
+//        self.cancellableForArtists = WebService.searchData(resource: Resource<Welcome3>(urlString: getArtistsUrlString(text)))
+//            .catch { _ in Just(Welcome3.placeholder())}
+//            .assign(to: \.artists, on: self)
+//        
+//        self.cancellableForPlaylists = WebService.searchData(resource: Resource<Welcome4>(urlString: getPlaylistsUrlString(text)))
+//            .catch { _ in Just(Welcome4.placeholder())}
+//            .assign(to: \.playlists, on: self)
+//        
+//    }
     
     func getImageUrl(urlString: String) -> URL? {
         //        "https://is2-ssl.mzstatic.com/image/thumb/Music124/v4/02/32/09/02320995-35fb-e63f-fb52-76595b70ed45/1.jpg/{w}x{h}bb.jpg"
@@ -182,11 +284,9 @@ class SearchViewModel: ObservableObject {
                 
                 guard let encodedString = path.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
                     return nil
-        //            fatalError("Invalid encodedString")
                 }
                 guard let url = URL(string: encodedString) else {
                     return nil
-        //            fatalError("Invalid URL")
                 }
                 return url
             }
@@ -209,124 +309,6 @@ extension SearchViewModel {
     }
     
 }
-class CombineViewModel: ObservableObject {
-    
-    private var cancellable: AnyCancellable?// View에 선언할 경우 내부에서 바뀌어야하는데 그러려면 state등을 해서 선언해야함
-    
-    @Published var welcome: WelcomeHints = WelcomeHints.placeholder()//모델에다가
-    
-    func fetchData(text: String) {
-        self.cancellable = WebService.getDataWithCombine(text: text)
-            .catch { _ in Just(WelcomeHints.placeholder())}
-            .assign(to: \.welcome, on: self)//아래거 가능//class만 가능하므로 뷰모델이 class.
-//            .sink(receiveCompletion: {_ in}, receiveValue: { welcome in
-//                self.welcome = welcome
-//                print(welcome)
-//            })
-    }
-}
 
-class CombineViewModel4: ObservableObject {
-    
-    private var cancellable: AnyCancellable?// View에 선언할 경우 내부에서 바뀌어야하는데 그러려면 state등을 해야하므로 좀 이상
-    
-    @Published var welcome: Welcome4 = Welcome4.placeholder()//모델에다가
-    
-    func fetchData(text: String) {
-        self.cancellable = WebService.getDataWithCombinePlaylists(text: text)
-            .catch { _ in Just(Welcome4.placeholder())}
-            .print("?")
-            .assign(to: \.welcome, on: self)//아래거 가능//class만 가능하므로 뷰모델이 class.
 
-    }
-    
-    func getImageUrl(index: Int) -> URL? {
-        //        "https://is2-ssl.mzstatic.com/image/thumb/Music124/v4/02/32/09/02320995-35fb-e63f-fb52-76595b70ed45/1.jpg/{w}x{h}bb.jpg"
-                var path  = welcome.results.suggestions[index].content.attributes.artwork.url
-                path = path.replacingOccurrences(of: "{w}", with: "100")
-                path = path.replacingOccurrences(of: "{h}", with: "100")
-                
-                guard let encodedString = path.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-                    return nil
-        //            fatalError("Invalid encodedString")
-                }
-                guard let url = URL(string: encodedString) else {
-                    return nil
-        //            fatalError("Invalid URL")
-                }
-                return url
-            }
-    
-}
-
-class CombineViewModel3: ObservableObject {
-    
-    private var cancellable: AnyCancellable?// View에 선언할 경우 내부에서 바뀌어야하는데 그러려면 state등을 해야하므로 좀 이상
-    
-    @Published var welcome: Welcome3 = Welcome3.placeholder()//모델에다가
-    
-    func fetchData(text: String) {
-        self.cancellable = WebService.getDataWithCombineArtists(text: text)
-            .catch { _ in Just(Welcome3.placeholder())}
-            .print()
-            .assign(to: \.welcome, on: self)//아래거 가능//class만 가능하므로 뷰모델이 class.
-//            .sink(receiveCompletion: {_ in}, receiveValue: { welcome in
-//                self.welcome = welcome
-//                print(welcome)
-//            })
-    }
-    
-    func getImageUrl(index: Int) -> URL? {
-        //        "https://is2-ssl.mzstatic.com/image/thumb/Music124/v4/02/32/09/02320995-35fb-e63f-fb52-76595b70ed45/1.jpg/{w}x{h}bb.jpg"
-                var path  = welcome.results.suggestions[index].content.attributes.artwork.url
-                path = path.replacingOccurrences(of: "{w}", with: "100")
-                path = path.replacingOccurrences(of: "{h}", with: "100")
-                
-                guard let encodedString = path.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-                    return nil
-        //            fatalError("Invalid encodedString")
-                }
-                guard let url = URL(string: encodedString) else {
-                    return nil
-        //            fatalError("Invalid URL")
-                }
-                return url
-            }
-    
-    
-}
-
-class CombineViewModel2: ObservableObject {
-    
-    private var cancellable: AnyCancellable?// View에 선언할 경우 내부에서 바뀌어야하는데 그러려면 state등을 해야하므로 좀 이상
-    
-    @Published var welcome: Welcome2 = Welcome2.placeholder()//모델에다가
-    
-    func getImageUrl(index: Int) -> URL? {
-        var path  = welcome.results.suggestions[index].content.attributes.artwork.url
-        
-        path = path.replacingOccurrences(of: "{w}", with: "100")
-        path = path.replacingOccurrences(of: "{h}", with: "100")
-        
-        guard let encodedString = path.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-            return nil
-//            fatalError("Invalid encodedString")
-        }
-        guard let url = URL(string: encodedString) else {
-            return nil
-//            fatalError("Invalid URL")
-        }
-        return url
-    }
-    func fetchData(text: String) {
-        self.cancellable = WebService.getDataWithCombineSongs(text: text)
-            .catch { _ in Just(Welcome2.placeholder())}
-            .print()
-            .assign(to: \.welcome, on: self)//아래거 가능//class만 가능하므로 뷰모델이 class.
-//            .sink(receiveCompletion: {_ in}, receiveValue: { welcome in
-//                self.welcome = welcome
-//                print(welcome)
-//            })
-    }
-}
 
