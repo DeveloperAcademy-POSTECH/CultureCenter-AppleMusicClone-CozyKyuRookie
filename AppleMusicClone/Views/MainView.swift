4a23b92cc691df6d9c93ee82ce46fce22bfb3ecf
hankyulee <77485339+kyustudyo@users.noreply.github.com>
2022-07-05 15:47:23 +0900

    #6 feat: tabView UI
    accentColor = .systemRed

---
+++ AppleMusicClone/Views/MainView.swift
@@ -0,0 +1,46 @@
+//
+//  MainView.swift
+//  AppleMusicClone
+//
+//  Created by Hankyu Lee on 2022/07/05.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            
+            //TODO: 각 뷰 넣기
+            Text("1")
+                .tabItem {
+                    Image(systemName: "play.circle.fill")
+                    Text("지금 듣기")
+                }
+            
+            Text("2")
+                .tabItem {
+                    Image(systemName: "dot.radiowaves.left.and.right")
+                    Text("라디오")
+                }
+
+            Text("3")
+                .tabItem {
+                    Image(systemName: "play.circle.fill")
+                    Text("지금 듣기")
+                }
+
+            Text("4")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("검색")
+                }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
